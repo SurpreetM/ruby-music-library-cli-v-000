864aa96df68fe19fcf08b72780dec0c8cb4f9123
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-09-07 14:27:39 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,61 @@
+class Song
+
+  attr_accessor :name
+
+  #Only need a getter method for artist as we've set a custom setter method below
+  attr_reader :artist, :genre
+
+  @@all = []
+
+# initialize with an optional artist & genre
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    # if artist is not set tp blank only then run the Song.artist= method
+    if artist != nil
+      self.artist = artist
+    end
+
+    if genre != nil
+      self.genre = genre
+    end
+
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    all.clear
+  end
+
+  def self.create(name)
+      song = Song.new(name)
+      song.save
+      song
+  end
+
+# Custom setter method for genre
+# Also runs the .add_song method from the artist and genre - to ensure the song it included in the artist and genres array.
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.add_song(self)
+  end
+
+  def find_by_name(song)
+    self.all.find do |song|
+      song.name == song
+    end
+  end
+
+
+end
