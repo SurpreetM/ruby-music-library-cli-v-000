43090892ead0a8a842a44b17637b3dfb05ce61d5
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-09-07 15:18:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -75,10 +75,10 @@ class Song
     artist = Artist.find_or_create_by_name(artist_name)
     genre = Genre.find_or_create_by_name(genre_name)
 
-    self.new(song_name, artist, genre)  
+    self.new(song_name, artist, genre)
   end
 
-  def create_from_filename(filename)
+  def self.create_from_filename(filename)
     new_from_filename(filename)
     save
   end
